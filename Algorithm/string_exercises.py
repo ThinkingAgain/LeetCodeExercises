a00e9599b20ecf9e6f9941d97a4e3d1cfeaacf98
ThinkingAgain <48943485+ThinkingAgain@users.noreply.github.com>
2019-07-20 16:03:18 +0800

    新增：回文字符串验证

--- Algorithm/string_exercises.py
+++ Algorithm/string_exercises.py
@@ -24,10 +24,29 @@ def isAnagram(s, t):
             return False
     return True
 
+"""
+ 验证回文字符串
+ ===============================================================================
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+说明：本题中，我们将空字符串定义为有效的回文串。
+-----------------------------------------
+示例 1:
+输入: "A man, a plan, a canal: Panama"
+输出: true
+--------------------------------------
+示例 2:
+输入: "race a car"
+输出: false
+--------------------------------------
+"""
+def isPalindrome(s: str) -> bool:
+    isp = "".join([x for x in s if x.isalnum()]).lower()
+    return isp == isp[::-1]
+
+
 
 
 
 if __name__ == "__main__":
-    s = "rat"
-    t = "car"
-    print(isAnagram(s, t))
+    s = "0p"
+    print(isPalindrome(s))
