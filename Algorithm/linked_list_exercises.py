2c049de5897478d750c86c3a37989fe9d7952a3c
ThinkingAgain <48943485+ThinkingAgain@users.noreply.github.com>
2019-07-24 19:38:04 +0800

    链表continue...

--- Algorithm/linked_list_exercises.py
+++ Algorithm/linked_list_exercises.py
@@ -4,7 +4,28 @@ class ListNode:
         self.val = x
         self.next = None
 
+    def __str__(self):
+        ite = self
+        tostring = str(ite.val)
+        while ite.next != None:
+            tostring += "->"+str(ite.next.val)
+            ite = ite.next
+        return tostring
+
 class Solution:
+    #用列表构造一个链表
+    def constructNodelist(self, li:list)->ListNode:
+        if (len(li) == 0):
+            return None
+        head = ListNode(li[0])
+        tail = head
+        for x in li[1:]:
+            tail.next = ListNode(x)
+            tail = tail.next
+        return head
+
+    #返回链表字符串
+
     """
     删除链表中的节点
     ==================================
@@ -46,19 +67,42 @@ class Solution:
         prev.next = prev.next.next
         return head
 
+    """
+    反转链表
+    =============================
+    反转一个单链表。
+    示例:    
+    输入: 1->2->3->4->5->NULL
+    输出: 5->4->3->2->1->NULL
+    进阶:
+    你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+    """
+    def reverseList_iterator(self, head: ListNode) -> ListNode:
+        if head == None:
+            return None
+        if head.next == None:
+            return head
+        newhead = head
+        head = head.next
+        newhead.next = None
+        while head.next != None:
+            oldhead = head.next
+            head.next = newhead
+            newhead = head
+            head = oldhead
+        head.next = newhead
+        return head
+
+
+
+
 
 if __name__ == "__main__":
-    head = None
-    for i in range(5,0,-1):
-        x = ListNode(i)
-        x.next = head
-        head = x
-    """
-        """
     solution = Solution()
-    solution.removeNthFromEnd(head,2)
-    while head != None:
-        print(head.val)
-        head = head.next
+
+    li = [1, 2, 3, 4, 5]
+    head = solution.constructNodelist(li)
+    head = solution.reverseList_iterator(head)
+    print(head)
 
 
